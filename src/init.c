b50a97aa1c2d1b425549df87e99a13edd005825a
curtyraissa <raissacurty@gmail.com>
2025-02-22 12:45:23 +0000

    update

--- src/init.c
+++ src/init.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcurty-g <rcurty-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: raissacurty <raissacurty@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 13:06:51 by rcurty-g          #+#    #+#             */
-/*   Updated: 2025/02/11 16:30:26 by rcurty-g         ###   ########.fr       */
+/*   Updated: 2025/02/15 13:48:03 by raissacurty      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+// Initialize the game structure by setting all values to zero.
 void	init_game(t_map *game)
 {
 	ft_memset(game, 0, sizeof(t_map));
@@ -20,6 +21,8 @@ void	init_game(t_map *game)
 	ft_memset(&game->image, 0, sizeof(t_image));
 }
 
+// Allocate memory for the game structure, initialize its values (init_game),
+// and start the MiniLibX library with mlx_init().
 t_map	*init(void)
 {
 	t_map	*game;
